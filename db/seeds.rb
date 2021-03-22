e7d0d7ef71b17a036d4e9d0681017e20bc4db230
Sam Fretwell <sam.fretwell@commit.digital>
2021-03-22 18:19:10 +0000

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
 Animal.delete_all
+FeedingRecord.delete_all
+WeightRecord.delete_all
 
-Animal.create!(
+jeff = Animal.create!(
   name: "Jeff",
   scientific_name: "Python Regius",
   common_name: "Royal Python",
@@ -11,4 +13,18 @@ Animal.create!(
   birthday: Date.new(2016, 1, 1),
   sex: Animal::MALE,
   description: "A royal called jeff. lovely."
+)
+
+FeedingRecord.create!(
+  food: "Large rat",
+  recorded_on: Date.today,
+  notes: "Left in overnight",
+  animal: jeff
+)
+
+WeightRecord.create!(
+  weight: 10.5,
+  recorded_on: Date.today,
+  notes: "Scales slightly off",
+  animal: jeff
 )
