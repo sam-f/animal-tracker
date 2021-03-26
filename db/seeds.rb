e53be5ec4faaa6e26704584afa32348e8cd5f547
Sam Fretwell <sam.fretwell@commit.digital>
2021-03-26 08:19:19 +0000

    Adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,35 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
+User.delete_all
+AnimalGroup.delete_all
 Animal.delete_all
 FeedingRecord.delete_all
 WeightRecord.delete_all
+CleaningRecord.delete_all
+StockList.delete_all
+StockListPlacement.delete_all
+
+alex = User.create!(
+  first_name: "Alex",
+  last_name: "Smith",
+  email: "alex@example.com",
+  password: "secret"
+)
+
+snakes = AnimalGroup.create!(
+  name: "Snakes",
+  user: alex
+)
+
+snake_stock_list = StockList.create!(
+  user: alex,
+  name: "First clutch '21",
+  available_from: Date.today
+)
 
 jeff = Animal.create!(
+  animal_group: snakes,
   name: "Jeff",
   scientific_name: "Python Regius",
   common_name: "Royal Python",
@@ -13,6 +37,13 @@ jeff = Animal.create!(
   birthday: Date.new(2016, 1, 1),
   sex: Animal::MALE,
   description: "A royal called jeff. lovely."
+)
+
+StockListPlacement.create!(
+  animal: jeff,
+  stock_list: snake_stock_list,
+  name: "Jeff CB20 Pastel Pied",
+  price: 120
 )
 
 FeedingRecord.create!(
@@ -26,5 +57,10 @@ WeightRecord.create!(
   weight: 10.5,
   recorded_on: Date.today,
   notes: "Scales slightly off",
+  animal: jeff
+)
+
+CleaningRecord.create!(
+  recorded_on: Date.today,
   animal: jeff
 )
