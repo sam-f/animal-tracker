2d271f75cce2d5a451aefe5a5dba26ff315e38de
Sam Fretwell <sam.fretwell@commit.digital>
2021-04-10 16:24:20 +0100

    Adds animal#optimised_image

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -23,12 +23,17 @@ class Animal < ApplicationRecord
   has_many :feeding_records, dependent: :delete_all
   has_many :cleaning_records, dependent: :delete_all
   has_many :stock_list_placements, dependent: :destroy_async
-  has_many_attached :photos
+  has_one_attached :photo
 
   # Validations
   validates :name,
     presence: true,
     length: {minimum: 2, maximum: 140}
-
   validates :sex, presence: true, inclusion: {in: SEXES}
+
+  def optimised_photo(format: :webp)
+    if photo.attached?
+      photo.variant(resize_to_fill: [400, 300].freeze, convert: format, gravity: :center)
+    end
+  end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Animal, type: :model do
     it { should have_many(:feeding_records).dependent(:delete_all) }
     it { should have_many(:cleaning_records).dependent(:delete_all) }
     it { should have_many(:stock_list_placements).dependent(:destroy_async) }
+    it { should have_one_attached(:photo) }
   end
 
   describe "validations" do
@@ -19,7 +20,36 @@ RSpec.describe Animal, type: :model do
     it { should validate_length_of(:name).is_at_least(2).is_at_most(140) }
   end
 
-  describe "associations" do
-    it { should have_many_attached(:photos) }
+  describe "#optimised_photo" do
+    let(:animal) { FactoryBot.build(:animal) }
+
+    context "photo attached" do
+      before do
+        photo = File.open(Rails.root.join("test-files", "bruno.jpg"))
+        animal.photo.attach(io: photo, filename: "bruno.jpg")
+      end
+
+      it "returns an active storage variant" do
+        expect(animal.optimised_photo).to be_a ActiveStorage::VariantWithRecord
+      end
+
+      it "has transformations" do
+        expect(animal.optimised_photo(format: :jpeg).variation.transformations).to eq({
+          convert: :jpeg, format: "jpg", gravity: :center, resize_to_fill: [400, 300]
+        })
+      end
+
+      it "defaults to webp" do
+        expect(animal.optimised_photo.variation.transformations).to eq({
+          convert: :webp, format: "jpg", gravity: :center, resize_to_fill: [400, 300]
+        })
+      end
+    end
+
+    context "photo not attached" do
+      it "returns nil" do
+        expect(animal.optimised_photo).to be_nil
+      end
+    end
   end
 end
