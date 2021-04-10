d3a6a04f0d3385449fef8538d5fee86cba0aea77
Sam Fretwell <sam.fretwell@commit.digital>
2021-04-10 16:23:21 +0100

    Lint js

--- app/javascript/dashboard/nav.ts
+++ app/javascript/dashboard/nav.ts
@@ -1,21 +1,21 @@
 export default () => {
-  const navEl: HTMLElement = document.querySelector('#animalsDropDown');
-  const html: HTMLElement = document.querySelector('html');
+  const navEl: HTMLElement = document.querySelector('#animalsDropDown')
+  const html: HTMLElement = document.querySelector('html')
 
   navEl.addEventListener('click', (event): void => {
     if (navEl.classList.contains('is-active')) {
-      navEl.classList.remove('is-active');
+      navEl.classList.remove('is-active')
     } else {
-      navEl.classList.add('is-active');
+      navEl.classList.add('is-active')
     }
 
-    event.stopPropagation();
-  });
+    event.stopPropagation()
+  })
 
   html.addEventListener('click', (event): void => {
     if (navEl.classList.contains('is-active')) {
-      navEl.classList.remove('is-active');
+      navEl.classList.remove('is-active')
     }
-    event.stopPropagation();
-  });
-};
+    event.stopPropagation()
+  })
+}
