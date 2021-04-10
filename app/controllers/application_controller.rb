f1dff83e1685d8565f831d1edd844d5543340193
Sam Fretwell <sam.fretwell@commit.digital>
2021-04-10 16:07:48 +0100

    Renders not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   def not_found(exception = nil)
     respond_to do |format|
-      format.html { render "404", status: 404 }
+      format.html { render "not_found", status: 404 }
       format.any { head 404 }
     end
   end
